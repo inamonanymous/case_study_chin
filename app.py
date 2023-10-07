5064570ab0eaff13757e2e50ae1344343c2686ac
Stephen Aguilar <stephenonline25@gmail.com>
2023-10-08 05:46:57 +0800

    added calendar route, database already set

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, session, url_for
+from model import db, Users
+import calendar
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/db_event_management'
+app.secret_key = 'putanginamochin'
+db.init_app(app)
+
+def generate_calendar(year, month):
+    cal = calendar.month(year, month)
+    return cal
+
+@app.route('/calendar')
+def display_calendar():
+    months = []
+    
+    # Generate calendars for January to December of a specific year (e.g., 2023)
+    year = 2023
+    for month in range(1, 13):
+        cal = generate_calendar(year, month)
+        months.append({'month_name': calendar.month_name[month], 'calendar': cal})
+
+    return render_template('events-calendar.html', months=months)
+
+@app.route('/option/<option>')
+def option(option):
+    if 'email' in session:
+        return render_template('dashboard.html', selected_option=option)
+    return redirect(url_for('index'))
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    if 'email' in session:
+        selected_option = request.args.get('option', 'option1')
+        return render_template('dashboard.html', selected_option=selected_option)
+    return redirect(url_for('index'))
+
+@app.route('/authenticate', methods=['POST', 'GET'])
+def authenticate():
+    email, password = request.form.get('email'), request.form.get('password')
+    if Users.login_is_true(email, password):
+        session['email'] = email
+        return redirect(url_for('dashboard'))
+    return redirect(url_for('index'))
 
 @app.route('/') 
 def index():
-    return f"hi"
+    session.clear()
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    pwd = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(250), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(10), nullable=False)
+    status = db.Column(db.String(10), nullable=False)
+    bdate = db.Column(db.String(100), nullable=False)
+
+    @classmethod
+    def login_is_true(cls, email, password) -> bool:
+        user_obj = cls.query.filter_by(email=email).first()
+        return user_obj and password == user_obj.pwd
+
+class Reservations(db.Model):
+    __tablename__ = 'reservations'
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, nullable=False)
+    ucount = db.Column(db.Integer, nullable=False)
+    rdate = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(10), nullable=False)
+    comments = db.Column(db.String(250), nullable=False)
+    bdate = db.Column(db.String(100), nullable=False)
+
+class Holidays(db.Model):
+    __tablename__ = 'holidays'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(20), nullable=False)
+    reason = db.Column(db.String(100), nullable=False)
+    bdate = db.Column(db.String(100), nullable=False)
+
+class Frontdesk(db.Model):
+    __tablename__ = 'frontdest_users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    pwd = db.Column(db.String(200), nullable=False)
+    bdate = db.Column(db.String(100), nullable=False)
